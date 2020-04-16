df755a09fbf3005bd80418444755ffa9dccada04
trevor <tloveday@hawk.iit.edu>
2020-04-15 19:51:23 -0500

    Add title command and variable for powerhsell directory

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,20 +8,23 @@ New-Alias -Name vim -Value 'C:\Program Files (x86)\vim\vim80\vim.exe'
 Write-Host 'Powershell' $PsVersionTable.PSVersion '-' (Get-date)
 Write-Host ''
 
+#Functions for git:::::status/diff/add/reset/commit/push/lazy/pull up&master
 function gits {git status}
 function gitd {git diff}
 function gita {git add .}
 function gitr {git reset HEAD}
 function gitc([string]$a) {git commit -m $a}
 function gitp {git push origin}
-function gitdone { gita; gitp}
 function gitlazy([string]$a) {gita; 
 git commit -m $a; 
 gitp}
 function gitpud {git pull upstream dev}
-
 function gitpum {git pull upstream master}
 
+#change title of powershell command
+function title([string]$a){ $host.UI.RawUI.WindowTitle = $a}
+
+#Cool logo to start off the banner thingy 
 Write-Host '      _________'
 Write-Host '     / ======= \'
 Write-Host '    / __________\'
@@ -34,5 +37,12 @@ Write-Host '   / """"""""""" \                       /'
 Write-Host '  / ::::::::::::: \                  =D-`'
 Write-Host ' (_________________)'
 
-Set-Location -Path D:\School\IIT-Semester-5-Spring\
+# Starting Location
+set-Location -Path D:\School\IIT-Semester-5-Spring\
+
+#move this this directory to git stuff
+$PSDIR = "C:\Users\Trevor\Documents\PowerShell"
+
+# This changes the title of your powershelll
+$host.UI.RawUI.WindowTitle = "Trevors Powershell"ERSHELLDIR = "C:\Users\Trevor\Documents\PowerShell"
 
